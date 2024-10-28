72471ef4e614daf30700b9986954be52f1e09f9c
PranitRout07 <pranitrout72@gmail.com>
2024-10-28 07:52:14 +0530

    show all the products by fetching from database,button to add and remove product from cart

--- crud-using-react-golang-and-postgres/client/src/components/Products.jsx
+++ crud-using-react-golang-and-postgres/client/src/components/Products.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
 
 // All the products are fetched from the database and shown here . Here the product have an image, button to add to cart, also you can see the price of the product and a small description of the product, and also the title of the product .
 
@@ -10,57 +11,59 @@ import React, { useState } from 'react'
 // 5. Add to cart button
 
 function Products() {
-
+  
   const [products,setProducts] = useState([
     {
-      productTitle:"Acer Predator",
-      productDescr:"It is a gaming laptop that runs all the games smoothly.Ih has 24 processors with 16 GB RAM and 4055 GPU. It comes with a 330 watt battery which can recharge the laptop very quickly.",
-      productImage:"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQyVpfAVukAYMC_9sb1ai4A9vBTx4uUSH0a-VQoi3AFqi2j7qmk0e8j4UKUsB1O3_hzlwCZbSnzAu0aXF8B4Xg75V9lv_DO-FtRfqBRakglizVxFZOXEK1cYw",
-      productPrice:150000
-    },
-    {
-      productTitle:"Acer Predator",
-      productDescr:"It is a gaming laptop that runs all the games smoothly.Ih has 24 processors with 16 GB RAM and 4055 GPU. It comes with a 330 watt battery which can recharge the laptop very quickly.",
-      productImage:"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQyVpfAVukAYMC_9sb1ai4A9vBTx4uUSH0a-VQoi3AFqi2j7qmk0e8j4UKUsB1O3_hzlwCZbSnzAu0aXF8B4Xg75V9lv_DO-FtRfqBRakglizVxFZOXEK1cYw",
-      productPrice:150000
-    },
-    {
-      productTitle:"Acer Predator",
-      productDescr:"It is a gaming laptop that runs all the games smoothly.Ih has 24 processors with 16 GB RAM and 4055 GPU. It comes with a 330 watt battery which can recharge the laptop very quickly.",
-      productImage:"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQyVpfAVukAYMC_9sb1ai4A9vBTx4uUSH0a-VQoi3AFqi2j7qmk0e8j4UKUsB1O3_hzlwCZbSnzAu0aXF8B4Xg75V9lv_DO-FtRfqBRakglizVxFZOXEK1cYw",
-      productPrice:150000
-    },
-    {
-      productTitle:"Acer Predator",
-      productDescr:"It is a gaming laptop that runs all the games smoothly.Ih has 24 processors with 16 GB RAM and 4055 GPU. It comes with a 330 watt battery which can recharge the laptop very quickly.",
-      productImage:"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQyVpfAVukAYMC_9sb1ai4A9vBTx4uUSH0a-VQoi3AFqi2j7qmk0e8j4UKUsB1O3_hzlwCZbSnzAu0aXF8B4Xg75V9lv_DO-FtRfqBRakglizVxFZOXEK1cYw",
-      productPrice:150000
-    },
-    {
-      productTitle:"Acer Predator",
-      productDescr:"It is a gaming laptop that runs all the games smoothly.Ih has 24 processors with 16 GB RAM and 4055 GPU. It comes with a 330 watt battery which can recharge the laptop very quickly.",
-      productImage:"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQyVpfAVukAYMC_9sb1ai4A9vBTx4uUSH0a-VQoi3AFqi2j7qmk0e8j4UKUsB1O3_hzlwCZbSnzAu0aXF8B4Xg75V9lv_DO-FtRfqBRakglizVxFZOXEK1cYw",
-      productPrice:150000
-    },
-    {
-      productTitle:"Acer Predator",
-      productDescr:"It is a gaming laptop that runs all the games smoothly.Ih has 24 processors with 16 GB RAM and 4055 GPU. It comes with a 330 watt battery which can recharge the laptop very quickly.",
-      productImage:"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQyVpfAVukAYMC_9sb1ai4A9vBTx4uUSH0a-VQoi3AFqi2j7qmk0e8j4UKUsB1O3_hzlwCZbSnzAu0aXF8B4Xg75V9lv_DO-FtRfqBRakglizVxFZOXEK1cYw",
-      productPrice:150000
-    },
-    {
-      productTitle:"Acer Predator",
-      productDescr:"It is a gaming laptop that runs all the games smoothly.Ih has 24 processors with 16 GB RAM and 4055 GPU. It comes with a 330 watt battery which can recharge the laptop very quickly.",
-      productImage:"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQyVpfAVukAYMC_9sb1ai4A9vBTx4uUSH0a-VQoi3AFqi2j7qmk0e8j4UKUsB1O3_hzlwCZbSnzAu0aXF8B4Xg75V9lv_DO-FtRfqBRakglizVxFZOXEK1cYw",
-      productPrice:150000
-    },
-  ])
+      productId:"",
+      productTitle:"",
+      productDescr:"",
+      productImage:"",
+      productPrice:0,
+      productIncart:""
+    },])
+    async function GetAllProducts(){
+      const resp = await axios.get("/api/products")
+      if(resp.status===200){
+        console.log(resp.data)
+        setProducts(resp.data.length>0 && resp.data.map(product => ({
+          productId:product.id,
+          productTitle: product.title,
+          productDescr: product.description,
+          productImage: product.image,
+          productPrice: product.price,
+          productIncart: product.incart
+      })));
+  
+      } 
+    }
+  useEffect(()=>{
+    GetAllProducts()
+  },[])
+
+  async function addToCart(e){
+    e.preventDefault();
+    const resp = await axios.put(`/api/update/${e.target.value}`)
+    if(resp.data=="added to cart"){
+          GetAllProducts();   
+    }
+  }
 
 
+  async function removeFromCart(e) {
+    e.preventDefault();
+    const resp = await axios.put(`/api/remove/${e.target.value}`)
+    if(resp.data=="removed from cart"){
+          GetAllProducts();   
+    }
+  }
+
+  console.log(products.length)
   return (
     <div className='overflow-y-scroll overflow-x-hidden h-screen '>
+      {products.length>0?<div>
+
       {
-        products.map((product,index)=>{
+        products.length>0 && products.map((product,index)=>{
           return (
             <div key={index} className='h-[146px] w-[600px] bg-white m-4 p-2 inline-block'>
               
@@ -89,7 +92,7 @@ function Products() {
                   </div>
 
                       <div className='flex justify-end mt-1'>
-                      <button className=' px-6 py-[2px] bg-orange-500 hover:bg-orange-800 text-white font-semibold hover:scale-105 transition-all duration-700 rounded-lg'>Add to cart</button>
+                      {product.productIncart===false?<button onClick={addToCart} value={product.productId} className=' px-6 py-[2px] bg-orange-500 hover:bg-orange-800 text-white font-semibold hover:scale-105 transition-all duration-700 rounded-lg'>Add to cart</button>:<button onClick={removeFromCart} value={product.productId} className=' px-6 py-[2px] bg-red-500 hover:bg-red-800 text-white font-semibold hover:scale-105 transition-all duration-700 rounded-lg'>Remove from cart</button>}
                   </div>
                   
 
@@ -98,6 +101,8 @@ function Products() {
           )
         })
       }
+      </div >:<div className='h-screen flex justify-center items-center '> <span className='text-5xl text-white font-semibold '>No products in cart</span></div>}
+
     </div>
   )
 }
