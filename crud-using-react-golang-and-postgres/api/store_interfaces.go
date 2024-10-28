ef3b4e2c37bfa78b81550f058167015e22c0290b
PranitRout07 <pranitrout72@gmail.com>
2024-10-28 07:46:59 +0530

    added methods to interface and modified the product structure

--- crud-using-react-golang-and-postgres/api/store_interfaces.go
+++ crud-using-react-golang-and-postgres/api/store_interfaces.go
@@ -2,5 +2,8 @@ package main
 
 type Storage interface{
 	getallproducts()(*[]Product,error)
+	addToCart(int)(*Product,error)
+	removeFromCart(int)(*Product,error)
+	getCartproducts()(*[]Product,error)
 
 }
--- crud-using-react-golang-and-postgres/api/types.go
+++ crud-using-react-golang-and-postgres/api/types.go
@@ -1,1 +1,11 @@
-package main 
+package main
+
+type Product struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Image       string `json:"image"`
+	Description string `json:"description"`
+	Price       int    `json:"price"`
+	Incart      bool   `json:"incart"`
+}
+
